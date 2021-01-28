41c8291114465413d10fb5d3bb9e079e97672865
Abhijit Kalta <abhijitkalta@gmail.com>
2021-01-28 17:11:54 +0530

    Add guestlist state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Parent from "./props/Parent";
+import GuestList from "./state/GuestList";
 
 function App() {
   return (
     <div className="App">
       <Parent />
+      <GuestList />
     </div>
   );
 }
